42818f0ded790a80cd93e3f75d947e6b3f1052e6
laasan <36587649+laasan@users.noreply.github.com>
2018-07-18 13:42:35 +0300

    версия 2.4
    стиль ячейки время стал временем, а не общим(что вызывало неприятие макросов)

---
+++ SEM03.java
@@ -0,0 +1,354 @@
+public class SEM03 extends EQM06{
+    String NN,TradeNo,BoardId,BoardName,TradeDate,SettleDate,ShortName,SecurityId,BuySell,OrderNo,sTime,TrdType,Price,Quantity,Value,AccInt,Amount,Commission,CurrencyId,TrdAccId,CPFirmId,CPTrdAccId,Yield,Period,SettleCode,BrokerRef,ExtRef,UserId,Price2,AccInt2,RepoRate,RepoPeriod,FirmId,RepoValue,Discount,UpperDiscount,LowerDiscount,MatchRef,ClientCode,Details,SubDetails,CpFirmINN,FaceValue,RefundRate;
+
+    public String getNN() {
+        if(this.NN instanceof String) return NN;
+        else return "";
+    }
+    public void setNN(String nN) {
+        NN = nN;
+    }
+
+    public String getTradeNo() {
+        if(this.TradeNo instanceof String) return TradeNo;
+        else return "";
+    }
+    public void setTradeNo(String tradeNo) { TradeNo = tradeNo; }
+
+    public String getBoardId() {
+        if(this.BoardId instanceof String) return BoardId;
+        else return "";
+    }
+    public void setBoardId(String boardId) {
+        BoardId = boardId;
+    }
+
+    public String getBoardName() {
+        if(this.BoardName instanceof String) return BoardName;
+        else return "";
+    }
+    public void setBoardName(String boardName) {
+        BoardName = boardName;
+    }
+
+    public String getTradeDate() {
+        if(this.TradeDate instanceof String) return TradeDate;
+        else return "";
+    }
+    public void setTradeDate(String tradeDate) {
+        TradeDate = tradeDate;
+    }
+
+    public String getSettleDate() {
+        if(this.SettleDate instanceof String) return SettleDate;
+        else return "";
+    }
+    public void setSettleDate(String settleDate) {
+        SettleDate = settleDate;
+    }
+
+    public String getShortName() {
+        if(this.ShortName instanceof String) return ShortName;
+        else return "";
+    }
+    public void setShortName(String shortName) {
+        ShortName = shortName;
+    }
+
+    public String getSecurityId() {
+        if(this.SecurityId instanceof String) return SecurityId;
+        else return "";
+    }
+    public void setSecurityId(String securityId) {
+        SecurityId = securityId;
+    }
+
+    public String getBuySell() {
+        if(this.BuySell instanceof String) return BuySell;
+        else return "";
+    }
+    public void setBuySell(String buySell) {
+        BuySell = buySell;
+    }
+
+    public String getOrderNo() {
+        if(this.OrderNo instanceof String) return OrderNo;
+        else return "";
+    }
+    public void setOrderNo(String orderNo) {
+        OrderNo = orderNo;
+    }
+
+    public String getsTime() {
+        if(this.sTime instanceof String) return sTime;
+        else return "";
+    }
+    public void setsTime(String stime) {
+        sTime = stime;
+    }
+
+    public String getTrdType() {
+        if(this.TrdType instanceof String) return TrdType;
+        else return "";
+    }
+    public void setTrdType(String trdType) {
+        TrdType = trdType;
+    }
+
+    public String getPrice() {
+        if(this.Price instanceof String) return Price;
+        else return "0";
+    }
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getQuantity() {
+        if(this.Quantity instanceof String) return Quantity;
+        else return "0";
+    }
+    public void setQuantity(String quantity) {
+        Quantity = quantity;
+    }
+
+    public String getValue() {
+        if(this.Value instanceof String) return Value;
+        else return "0";
+    }
+    public void setValue(String value) {
+        Value = value;
+    }
+
+    public String getAccInt() {
+        if(this.AccInt instanceof String) return AccInt;
+        else return "0";
+    }
+    public void setAccInt(String accInt) {
+        AccInt = accInt;
+    }
+
+    public String getAmount() {
+        if(this.Amount instanceof String) return Amount;
+        else return "0";
+    }
+    public void setAmount(String amount) {
+        Amount = amount;
+    }
+
+    public String getCommission() {
+        if(this.Commission instanceof String) return Commission;
+        else return "0";
+    }
+    public void setCommission(String commission) {
+        Commission = commission;
+    }
+
+    public String getCurrencyId() {
+        if(this.CurrencyId instanceof String) return CurrencyId;
+        else return "";
+    }
+    public void setCurrencyId(String currencyId) {
+        CurrencyId = currencyId;
+    }
+
+    public String getTrdAccId() {
+        if(this.TrdAccId instanceof String) return TrdAccId;
+        else return "";
+    }
+    public void setTrdAccId(String trdAccId) {
+        TrdAccId = trdAccId;
+    }
+
+    public String getCPFirmId() {
+        if(this.CPFirmId instanceof String) return CPFirmId;
+        else return "";
+    }
+    public void setCPFirmId(String cPFirmId) {
+        CPFirmId = cPFirmId;
+    }
+
+    public String getCPTrdAccId() {
+        if(this.CPTrdAccId instanceof String) return CPTrdAccId;
+        else return "";
+    }
+    public void setCPTrdAccId(String cPTrdAccId) {
+        CPTrdAccId = cPTrdAccId;
+    }
+
+    public String getYield() {
+        if(this.Yield instanceof String) return Yield;
+        else return "";
+    }
+    public void setYield(String yield) {
+        Yield = yield;
+    }
+
+    public String getPeriod() {
+        if(this.Period instanceof String) return Period;
+        else return "";
+    }
+    public void setPeriod(String period) {
+        Period = period;
+    }
+
+    public String getSettleCode() {
+        if(this.SettleCode instanceof String) return SettleCode;
+        else return "";
+    }
+    public void setSettleCode(String settleCode) {
+        SettleCode = settleCode;
+    }
+
+    public String getBrokerRef() {
+        if(this.BrokerRef instanceof String) return BrokerRef;
+        else return "";
+    }
+    public void setBrokerRef(String brokerRef) {
+        BrokerRef = brokerRef;
+    }
+
+    public String getExtRef() {
+        if(this.ExtRef instanceof String) return ExtRef;
+        else return "";
+    }
+    public void setExtRef(String extRef) {
+        ExtRef = extRef;
+    }
+
+    public String getUserId() {
+        if(this.UserId instanceof String) return UserId;
+        else return "";
+    }
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getPrice2() {
+        if(this.Price2 instanceof String) return Price2;
+        else return "0";
+    }
+    public void setPrice2(String price2) {
+        Price2 = price2;
+    }
+
+    public String getAccInt2() {
+        if(this.AccInt2 instanceof String) return AccInt2;
+        else return "0";
+    }
+    public void setAccInt2(String accInt2) {
+        AccInt2 = accInt2;
+    }
+
+    public String getRepoRate() {
+        if(this.RepoRate instanceof String) return RepoRate;
+        else return "0";
+    }
+    public void setRepoRate(String repoRate) {
+        RepoRate = repoRate;
+    }
+
+    public String getRepoPeriod() {
+        if(this.RepoPeriod instanceof String) return RepoPeriod;
+        else return "0";
+    }
+    public void setRepoPeriod(String repoPeriod) {
+        RepoPeriod = repoPeriod;
+    }
+
+    public String getFirmId() {
+        if(this.FirmId instanceof String) return FirmId;
+        else return "";
+    }
+    public void setFirmId(String firmId) {
+        FirmId = firmId;
+    }
+
+    public String getRepoValue() {
+        if(this.RepoValue instanceof String) return RepoValue;
+        else return "0";
+    }
+    public void setRepoValue(String repoValue) {
+        RepoValue = repoValue;
+    }
+
+    public String getDiscount() {
+        if(this.Discount instanceof String) return Discount;
+        else return "0";
+    }
+    public void setDiscount(String discount) {
+        Discount = discount;
+    }
+
+    public String getUpperDiscount() {
+        if(this.UpperDiscount instanceof String) return UpperDiscount;
+        else return "0";
+    }
+    public void setUpperDiscount(String upperDiscount) {
+        UpperDiscount = upperDiscount;
+    }
+
+    public String getLowerDiscount() {
+        if(this.LowerDiscount instanceof String) return LowerDiscount;
+        else return "0";
+    }
+    public void setLowerDiscount(String lowerDiscount) {
+        LowerDiscount = lowerDiscount;
+    }
+
+    public String getMatchRef() {
+        if(this.MatchRef instanceof String) return MatchRef;
+        else return "";
+    }
+    public void setMatchRef(String matchRef) {
+        MatchRef = matchRef;
+    }
+
+    public String getClientCode() {
+        if(this.ClientCode instanceof String) return ClientCode;
+        else return "";
+    }
+    public void setClientCode(String clientCode) {
+        ClientCode = clientCode;
+    }
+
+    public String getDetails() {
+        if(this.Details instanceof String) return Details;
+        else return "";
+    }
+    public void setDetails(String details) {
+        Details = details;
+    }
+
+    public String getSubDetails() {
+        if(this.SubDetails instanceof String) return SubDetails;
+        else return "";
+    }
+    public void setSubDetails(String subDetails) {
+        SubDetails = subDetails;
+    }
+
+    public String getCpFirmINN() {
+        if(this.CpFirmINN instanceof String) return CpFirmINN;
+        else return "";
+    }
+    public void setCpFirmINN(String cpFirmINN) {
+        CpFirmINN = cpFirmINN;
+    }
+
+    public String getFaceValue() {
+        if(this.FaceValue instanceof String) return FaceValue;
+        else return "0";
+    }
+    public void setFaceValue(String faceValue) {
+        FaceValue = faceValue;
+    }
+
+    public String getRefundRate() {
+        if(this.RefundRate instanceof String) return RefundRate;
+        else return "0";
+    }
+    public void setRefundRate(String refundRate) {
+        RefundRate = refundRate;
+    }
+
+}
